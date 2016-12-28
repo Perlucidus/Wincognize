b31ed4b9846b39d11b686ee787d8493b85f94e0f
Perlucidus <cwildcat579@gmail.com>
2016-12-28 03:31:29 +0200

    SQLite
    Added database interaction

---
+++ Wincognize/Wincognize/Context.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+using Wincognize.Data;
+
+namespace Wincognize
+{
+    public class Context : ApplicationContext
+    {
+        public Context()
+        {
+            Application.ApplicationExit += OnApplicationExit;
+            /*using (DataConnection connection = Database.Instance.CreateConnection())
+            {
+                //connection.ExecuteNonQuery("create table test1(s varchar(20))");
+                //connection.ExecuteNonQuery("insert into test1 (s) values ('a')");
+                using (ResultSet rs = connection.ExecuteQuery("select * from test1"))
+                    while (rs.NextRow())
+                        Console.WriteLine(rs.Get<string>("s"));
+            }*/
+        }
+
+        private void OnApplicationExit(Object sender, EventArgs e)
+        {
+            Console.WriteLine("Application Exit");
+        }
+    }
+}
---
+++ Wincognize/Wincognize/Data/Database.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+
+namespace Wincognize.Data
+{
+    public class Database
+    {
+        public static Database Instance;
+
+        private const string FilePath = @"{0}\Wincognize";
+        private const string FileName = "data.sqlite";
+        private const int SqliteVersion = 3;
+
+        private string m_connection;
+
+        static Database()
+        {
+            Instance = new Database();
+        }
+
+        private Database()
+        {
+            string dir = string.Format(FilePath, Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            string path = $@"{dir}\{FileName}";
+            Directory.CreateDirectory(dir);
+            if (!File.Exists(path))
+                SQLiteConnection.CreateFile(path);
+            m_connection = $"Data Source={path};Version={SqliteVersion};";
+        }
+
+        public DataConnection CreateConnection()
+        {
+            return new DataConnection(m_connection);
+        }
+    }
+}
